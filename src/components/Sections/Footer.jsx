10eefff3fee88afb05ac96757f5ea088cda51a5a
santiac24 <104871424+santiac24@users.noreply.github.com>
2023-03-23 16:07:34 -0500

    Footer

--- src/components/Sections/Footer.jsx
+++ src/components/Sections/Footer.jsx
@@ -1,50 +1,168 @@
-import React from "react";
-import styled from "styled-components";
-import { Link } from "react-scroll";
-// Assets
-import LogoImg from "../../assets/svg/Logo";
+import { useState } from 'react';
+import styled from 'styled-components';
+import { Link } from 'react-scroll';
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+import { faArrowUp, faEnvelope } from '@fortawesome/free-solid-svg-icons';
+import { faFacebook, faInstagram, faTwitter } from '@fortawesome/free-brands-svg-icons';
+import { GiVikingHead } from "react-icons/gi";
 
-export default function Contact() {
+export const Footer = () => {
+  const [email, setEmail] = useState('');
+
+  const handleSubmit = (e) => {
+    e.preventDefault();
+    console.log('Email submitted:', email);
+    setEmail('');
+  };
 
   const getCurrentYear = () => {
     return new Date().getFullYear();
-  }
+  };
 
   return (
-    <Wrapper id="Contacto">
-      <div className="darkBg">
-        <div className="container">
-          <InnerWrapper className="flexSpaceCenter" style={{ padding: "30px 0" }}>
-            <Link className="flexCenter animate pointer" to="home" smooth={true} offset={-80}>
-              <LogoImg />
-              <h1 className="font15 extraBold whiteColor" style={{ marginLeft: "15px" }}>
-                Fanatic
-              </h1>
-            </Link>
-            <StyleP className="whiteColor font13">
-              © {getCurrentYear()} - <span className="purpleColor font13">Fanatic</span> All Right Reserved
-            </StyleP>
-
-            <Link className="whiteColor animate pointer font13" to="home" smooth={true} offset={-80}>
-              Back to top
-            </Link>
-          </InnerWrapper>
-        </div>
-      </div>
-    </Wrapper>
+    <FooterContainer>
+      <LogoContainer>
+        <GiVikingHead className="icon-footer" />
+        <LogoText>Valhalla</LogoText>
+      </LogoContainer>
+      <SocialContainer>
+        <SocialIcon href="#" target="_blank">
+          <FontAwesomeIcon icon={faInstagram} />
+        </SocialIcon>
+        <SocialIcon href="#" target="_blank">
+          <FontAwesomeIcon icon={faFacebook} />
+        </SocialIcon>
+        <SocialIcon href="#" target="_blank">
+          <FontAwesomeIcon icon={faTwitter} />
+        </SocialIcon>
+      </SocialContainer>
+      <EmailForm onSubmit={handleSubmit}>
+        <EmailInput
+          type="email"
+          placeholder="Ingresa tu correo electronico."
+          value={email}
+          onChange={(e) => setEmail(e.target.value)}
+          required
+        />
+        <EmailButton type="submit">
+          <FontAwesomeIcon icon={faEnvelope} />
+        </EmailButton>
+      </EmailForm>
+      <FooterLinks>
+        <BackToTopLink to="home" smooth={true} offset={-80}>
+          <FontAwesomeIcon icon={faArrowUp} />
+          Volver al inicio
+        </BackToTopLink>
+        <Copyright>
+          © {getCurrentYear()} - Valhalla Todos los derechos reservados
+        </Copyright>
+      </FooterLinks>
+    </FooterContainer>
   );
-}
+};
 
-const Wrapper = styled.div`
-  width: 100%;
+const FooterContainer = styled.footer`
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+  background-color: #101522;
+  padding: 50px 0;
+  color: #fff;
 `;
-const InnerWrapper = styled.div`
-  @media (max-width: 550px) {
-    flex-direction: column;
+
+const LogoContainer = styled.div`
+  display: flex;
+  align-items: center;
+  margin-bottom: 30px;
+`;
+
+const LogoImg = styled.img`
+  width: 30px;
+  height: 30px;
+  margin-right: 10px;
+`;
+
+const LogoText = styled.h1`
+  font-size: 1.5rem;
+  font-weight: 700;
+  margin: 0;
+`;
+
+const SocialContainer = styled.div`
+  display: flex;
+  justify-content: center;
+  margin-bottom: 30px;
+`;
+
+const SocialIcon = styled.a`
+  font-size: 1.25rem;
+  color: #fff;
+  margin: 0 10px;
+  transition: color 0.3s ease-in-out;
+
+  &:hover {
+    color: #4b59f7;
   }
 `;
-const StyleP = styled.p`
-  @media (max-width: 550px) {
-    margin: 20px 0;
+
+const EmailForm = styled.form`
+  display: flex;
+  justify-content: center;
+  margin-bottom: 30px;
+`;
+
+const EmailInput = styled.input`
+  width: 250px;
+  padding: 10px;
+  border: none;
+  border-radius: 5px 0 0 5px;
+  font-size: 1rem;
+  color: #000;
+`;
+
+const EmailButton = styled.button`
+  padding: 10px 15px;
+  background-color: #4b59f7;
+  border: none;
+  color: #fff;
+  border-radius: 0 5px 5px 0;
+  font-size: 1rem;
+  cursor: pointer;
+  transition: background-color 0.3s ease-in-out;
+
+  &:hover {
+    background-color: #fff;
+    color: #4b59f7;
   }
 `;
+const FooterLinks = styled.div`
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+`;
+
+const BackToTopLink = styled(Link)`
+  display: flex;
+  align-items: center;
+  color: #fff;
+  cursor: pointer;
+  margin-bottom: 16px;
+  text-decoration: none;
+  transition: color 0.3s ease-in-out;
+
+  &:hover {
+    color: #4b59f7;
+  }
+
+  & svg {
+    margin-right: 8px;
+  }
+`;
+
+const Copyright = styled.p`
+  font-size: 0.8rem;
+  color: #fff;
+  text-align: center;
+`;
+
+  
--- src/screens/Landing.jsx
+++ src/screens/Landing.jsx
@@ -5,7 +5,7 @@ import Services from "../components/Sections/Services";
 import Projects from "../components/Sections/Projects";
 import Pricing from "../components/Sections/Pricing";
 import CarruselSedes from "../components/Sections/CarruselSedes";
-import Footer from "../components/Sections/Footer";
+import { Footer } from "../components/Sections/Footer";
 
 export default function Landing() {
   return (
