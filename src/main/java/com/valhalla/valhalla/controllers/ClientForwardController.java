104d96a6e7bbb467561bb27e45a2d1cd49e3fe26
ortizjulian <julian.ortixs@gmail.com>
2023-04-01 13:51:06 -0500

    Spring Stucture + React

---
+++ src/main/java/com/valhalla/valhalla/controllers/ClientForwardController.java
@@ -0,0 +1,13 @@
+package com.valhalla.valhalla.controllers;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+
+@Controller
+public class ClientForwardController {
+
+    @GetMapping(value = "/**/{path:[^\\.]*}")
+    public String home() {
+        return "forward:/";
+    }
+}
---
+++ src/test/java/com/valhalla/valhalla/ValhallaApplicationTests.java
@@ -0,0 +1,13 @@
+package com.valhalla.valhalla;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ValhallaApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
