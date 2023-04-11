2710292d07cf88e6c0198802899b3cb51b5e2e0d
ortizjulian <julian.ortixs@gmail.com>
2023-04-10 23:42:46 -0500

    Ejemplo estructura

---
+++ src/main/java/com/valhalla/valhalla/services/SedesService.java
@@ -0,0 +1,12 @@
+package com.valhalla.valhalla.services;
+
+public class SedesService {
+    /*
+     * @Autowired
+     * private SedesRepository userRepository;
+     * 
+     * public List<Sedes> listarSedes(){
+     * (List<Sedes>)userRepository.findAll();
+     * }
+     */
+}
