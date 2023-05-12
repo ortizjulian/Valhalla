21d107484096a61e5ed0f78938977f72a47c0a8f
Joseale58 <70922734+Joseale58@users.noreply.github.com>
2023-05-11 23:02:32 -0500

    dataReverse

--- src/main/valhalla/src/data/data.tsx
+++ src/main/valhalla/src/data/data.tsx
@@ -1,41 +1,6 @@
 import React, { useEffect, useState } from "react";
 
-interface Evento {
-  event_id: number;
-  title: string;
-  start: Date;
-  end: Date;
-  description: string;
-  profesor: string;
-}
-
-const [clases, setClases] = useState<Evento[]>([]);
-
-useEffect(() => {
-  fetch("/clases")
-    .then((response) => response.json())
-    .then((data) => {
-
-      
-      const eventos: Evento[] = data.map((clase: any) => {
-
-        return {
-          event_id: clase.id_clases,
-          title: clase.nombre,
-          start: new Date(clase.fecha_inicio),
-          end: new Date(clase.fecha_final),
-          description: clase.descripcion,
-          profesor: clase.profesor.id,
-        };
-      });
-      
-      setClases(eventos);
-      
-    })
-    .catch((error) => console.error(error));
-}, []);
-
-/* let EVENTS = [
+let EVENTS = [
   {
     event_id: 1,
     title: "Salsa",
@@ -52,6 +17,6 @@ useEffect(() => {
     description: "Puras barritas",
     profesor: "Jose",
   },
-]; */
+];
 
 export default EVENTS;
